921cf625e7170a1d503b0d0d67e40dc3e5f689a4
ciprodakama <telinoiu.1796513@studenti.uniroma1.it>
2020-06-30 20:08:54 +0200

    Contatto client server

--- js/firstLogin.js
+++ js/firstLogin.js
@@ -1,24 +1,46 @@
+const base_url = 'http://localhost:3001';
+
+var url = base_url+'/categories';
+
 var max_Cat = 5;
 var count = 0;
 
+var body = {
+    "categories": []
+};
 //salveremo qui in un array gli id delle singole categorie da mandare poi al server
+
+function findId(id){
+    if(body.categories.includes(id)){
+        return body.categories.indexOf(id);
+    }
+    else{
+        return -1;
+    }
+}
 
 $(document).ready(function(){
     $(".list-group-item").click(function(){
+        console.log(body.categories);
+        console.log(count);
+        var id = $(this).attr('id');
         if(count < max_Cat){
             if(($(this).hasClass("active"))){
                 $(this).removeClass("active");
                 count--;
+                body.categories.splice(findId(id),1);
             }
             else{
                 $(this).addClass("active");
                 count++;
+                body.categories.push(id);
             }
         }
         else{
             if(($(this).hasClass("active"))){
                 $(this).removeClass("active");
                 count--;
+                body.categories.splice(findId(id),1);
             }
             else{
                 alert("Hai già selezionato 5 categorie!");
@@ -31,6 +53,18 @@ $(document).ready(function(){
         if(count < 1){
             alert("Seleziona almeno 1 categoria per poter proseguire!");
             return false;
+        }
+        else{
+            $.ajax({
+                url: url,
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(body),
+                }).done(function(){
+                    window.location.href = "http://localhost:5500/main.html";
+                }).fail(function(){
+                    alert("Errore! Prova a rimandare le tue categorie!");
+                })
         }
     });
 });
