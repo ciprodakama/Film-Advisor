0c0b61a870ceb19a0db31cace252ec349ffba9b8
ciprodakama <telinoiu.1796513@studenti.uniroma1.it>
2020-06-30 13:06:05 +0200

    moved files here

---
+++ frontend/js/playlist.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.ispeziona').click(function(){
+        //invia ris server
+        $('#dettaglioPlaylist').show();
+    });
+
+    $('.nascondi').click(function(){
+        $('#dettaglioPlaylist').hide();
+    });
+});
