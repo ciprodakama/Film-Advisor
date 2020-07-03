13ba395c5285c6bfa622bb39f068643ef67bdfa9
ciprodakama <telinoiu.1796513@studenti.uniroma1.it>
2020-07-03 15:59:28 +0200

    Aggiunta Mail Index

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,16 +2,21 @@
 
 const base_url = 'http://localhost:3001';
 
-var url = base_url+'/login';
-
-console.log(url);
+var url = base_url+'/login?mail=';
 
 $(document).ready(function() {
     $("#login").click(function(){
-        $.get(url, function(data) {
-            window.location.href = data;
-        }).fail(function(){
-            alert("Errore! Non è stato possibile autentificarsi con Google.");
-        });
+        var mail = $("#inputEmail4").val();
+        if(!$("#inputEmail4").val()){
+            alert("Per poter procedere devi inserire la tua mail!");
+        }
+        else{
+            $.get(url+mail, function(data) {
+                console.log(data);
+                window.location.href = data;
+            }).fail(function(){
+                alert("Errore! Non è stato possibile autentificarsi con Google.");
+            });
+        }
     });
 });
