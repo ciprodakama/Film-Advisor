57ba72728556da3b4cfa89fa2533751c7caa1abb
Francesco D'Orazio <dorazio.1806836@studenti.uniroma1.it>
2020-07-04 16:28:24 +0000

    Update backend/ws/index.js

--- backend/ws/index.js
+++ backend/ws/index.js
@@ -2,180 +2,285 @@ var server = require('ws').Server;
 var serv = new server({ port: 4000 });
 
 var mongoose = require('mongoose');
-var body_p = require('body-parser');
 var require = require("request");
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0-hjbla.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true,useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
-var entry = mongoose.Schema({
-    nome: { type: String},
-    found: {type: Number, default: 1}
-});
+let translate = (arr) => {
+    for (ob of arr)
+    {
+        if( ob.nome == 28)
+            ob.nome = "Azione";
+        if( ob.nome == 12)
+            ob.nome = "Avventura";
+        if( ob.nome == 16)
+            ob.nome = "Animazione";
+        if( ob.nome == 35)
+            ob.nome = "Commedia";
+        if( ob.nome == 80)
+            ob.nome = "Crime";
+        if( ob.nome == 99)
+            ob.nome = "Documentario";
+        if( ob.nome == 10751)
+            ob.nome = "Famiglia";
+        if( ob.nome == 14)
+            ob.nome = "Fantasy";
+        if( ob.nome == 36)
+            ob.nome = "Storia";
+        if( ob.nome == 27)
+            ob.nome = "Horror";
+        if( ob.nome == 10402)
+            ob.nome = "Musica";
+        if( ob.nome == 9648)
+            ob.nome = "Mistero";
+        if( ob.nome == 10749)
+            ob.nome = "Romance";
+        if( ob.nome == 878)
+            ob.nome = "Fantascienza";
+        if( ob.nome == 10770)
+            ob.nome = "televisione film";
+        if( ob.nome == 53)
+            ob.nome = "Thriller";
+        if( ob.nome == 10752)
+            ob.nome = "Guerra";
+        if( ob.nome == 37)
+            ob.nome = "Western";
+    }
+    return arr;
+}
+
+
+let insertionSort = (inputArr) => {
+    let length = inputArr.length;
+    for (let i = 1; i < length; i++) {
+        var key = {
+            nome: inputArr[i].nome,
+            trovata: inputArr[i].trovata
+        }
+        let j = i - 1;
+        while (j >= 0 && inputArr[j].trovata < key.trovata) {
+            inputArr[j + 1] = inputArr[j];
+            j = j - 1;
+        }
+        inputArr[j + 1] = key;
+    }
+    return inputArr;
+};
 
 serv.on('connection', function(ws) {
-    var r;
-    /* var body = {
-        "password": "password"
-    }; */
-    require.get({
-        url: "http://localhost:8888/user",
-        header: {
-            'Content-Type' : 'application/json; charset=utf-8'
-        },
-        //body: JSON.stringify(body)
-    }, (err, res, body ) => {
-        console.log("body \n"+ body);
-        r = body;
-    });
-    console.log("r " + r);
+
     ws.on('message', function(msg) {
-        var s = "**Server**";
+        var s = "**Server**: ";
         var col = s.fontcolor("blue");
         if( msg == "_AvgPlaylists")
         {
-            var i = 0, sum = 0;
-            for( var ob of r )
-            {
-                sum += r.playlist.length;
-                i++;
-            }
-            sum = sum / i;
-            ws.send(col + "Il numero di Playlist madio è: " + sum);
+            require.get({
+                url: "http://localhost:8888/user",
+                header: {
+                    'Content-Type' : 'application/json; charset=utf-8'
+                },
+            }, (err, res, body ) => {
+
+                if(err)
+                    console.log(err)
+
+                var i = 0, sum = 0;
+                for( var ob of JSON.parse(body) )
+                {
+                    sum += ob.playlist.length;
+                    i++;
+                }
+                sum = sum / i;
+                sum = Math.floor(sum)
+                ws.send(col + "Il numero di Playlist madio è: " + sum);
+            });
+            
         }
         else if( msg == "_AvgElements")
         {
-            var i = 0, sum = 0;
-            for( var ob of r )
-            {
-                for(var e of r.playlist)
-                sum += e.numero_elementi;
-                i++;
-            }
-            sum = sum / i;
-            ws.send(col + "Il numero di Elementi medio in una playist è: " + sum);
+            require.get({
+                url: "http://localhost:8888/user",
+                header: {
+                    'Content-Type' : 'application/json; charset=utf-8'
+                },
+            }, (err, res, body ) => {
+
+                if(err)
+                    console.log(err)
+
+                var i = 0, sum = 0;
+                for( var ob of JSON.parse(body) )
+                {
+                    for(var e of ob.playlist)
+                    {
+                        sum += e.numero_elementi;
+                        i++;
+                    }
+                }
+                sum = sum / i;
+                sum = Math.floor(sum)
+                ws.send(col + "Il numero medio di Elementi in una Playlist è: " + sum);
+            });
         }
         else if( msg == "_Categories")
         {
-            //18 categorie
-            var arr = [];
-            for(var ob of r)
-            {
-                if (ob.cat1)
+            require.get({
+                url: "http://localhost:8888/user",
+                header: {
+                    'Content-Type' : 'application/json; charset=utf-8'
+                },
+            }, (err, res, body ) => {
+
+                if(err)
+                    console.log(err)
+
+                var arr = [];
+                for(var ob of JSON.parse(body))
                 {
-                    var b = 0;
-                    for(var e of arr)
+                    if (ob.cat1 && ob.cat1 != 0 )
                     {
-                        if( e.nome == ob.cat1 )
+                        var b = 0;
+                        for(var e of arr)
                         {
-                            e.found++;
-                            b = 1;
+                            if( e.nome == ob.cat1 )
+                            {
+                                e.trovata++;
+                                b = 1;
+                            }
+                        }
+                        if( !b )
+                        {
+                            var name = ob.cat1;
+                            var entry = {
+                                nome: name,
+                                trovata: 1
+                            };
+                            arr.push(entry);
                         }
                     }
-                    if( !b )
+                    if (ob.cat2 && ob.cat2 != 0 )
                     {
-                        var name = ob.cat1;
-                        var entry = {
-                            nome: name,
-                            found: 1
-                        };
-                        arr.push(entry);
-                    }
-                }
-                if (ob.cat2)
-                {
-                    var b = 0;
-                    for(var e of arr)
-                    {
-                        if( e.nome == ob.cat2 )
+                        var b = 0;
+                        for(var e of arr)
                         {
-                            e.found++;
-                            b = 1;
+                            if( e.nome == ob.cat2 )
+                            {
+                                e.trovata++;
+                                b = 1;
+                            }
+                        }
+                        if( !b )
+                        {
+                            var name = ob.cat2;
+                            var entry = {
+                                nome: name,
+                                trovata: 1
+                            };
+                            arr.push(entry);
                         }
                     }
-                    if( !b )
+                    if (ob.cat3 && ob.cat3 != 0 )
                     {
-                        var name = ob.cat2;
-                        var entry = {
-                            nome: name,
-                            found: 1
-                        };
-                        arr.push(entry);
-                    }
-                }
-                if (ob.cat3)
-                {
-                    var b = 0;
-                    for(var e of arr)
-                    {
-                        if( e.nome == ob.cat3 )
+                        var b = 0;
+                        for(var e of arr)
                         {
-                            e.found++;
-                            b = 1;
+                            if( e.nome == ob.cat3 )
+                            {
+                                e.trovata++;
+                                b = 1;
+                            }
+                        }
+                        if( !b )
+                        {
+                            var name = ob.cat3;
+                            var entry = {
+                                nome: name,
+                                trovata: 1
+                            };
+                            arr.push(entry);
                         }
                     }
-                    if( !b )
+                    if (ob.cat4 && ob.cat4 != 0 )
                     {
-                        var name = ob.cat3;
-                        var entry = {
-                            nome: name,
-                            found: 1
-                        };
-                        arr.push(entry);
-                    }
-                }
-                if (ob.cat4)
-                {
-                    var b = 0;
-                    for(var e of arr)
-                    {
-                        if( e.nome == ob.cat4 )
+                        var b = 0;
+                        for(var e of arr)
                         {
-                            e.found++;
-                            b = 1;
+                            if( e.nome == ob.cat4 )
+                            {
+                                e.trovata++;
+                                b = 1;
+                            }
+                        }
+                        if( !b )
+                        {
+                            var name = ob.cat4;
+                            var entry = {
+                                nome: name,
+                                trovata: 1
+                            };
+                            arr.push(entry);
                         }
                     }
-                    if( !b )
+                    if (ob.cat5 && ob.cat5 != 0 )
                     {
-                        var name = ob.cat4;
-                        var entry = {
-                            nome: name,
-                            found: 1
-                        };
-                        arr.push(entry);
-                    }
-                }
-                if (ob.cat5)
-                {
-                    var b = 0;
-                    for(var e of arr)
-                    {
-                        if( e.nome == ob.cat5 )
+                        var b = 0;
+                        for(var e of arr)
                         {
-                            e.found++;
-                            b = 1;
+                            if( e.nome == ob.cat5 )
+                            {
+                                e.trovata++;
+                                b = 1;
+                            }
+                        }
+                        if( !b )
+                        {
+                            var name = ob.cat5;
+                            var entry = {
+                                nome: name,
+                                trovata: 1
+                            };
+                            arr.push(entry);
                         }
                     }
-                    if( !b )
-                    {
-                        var name = ob.cat5;
-                        var entry = {
-                            nome: name,
-                            found: 1
-                        };
-                        arr.push(entry);
-                    }
                 }
-            }
-            ws.send(col + "");
+                var sorted_array = insertionSort(arr);
+                var return_array = []
+                for(var i = 0; i < 5; i++)
+                {
+                    return_array.unshift(sorted_array[i]);
+                }
+                var return_array = translate(return_array);
+                ws.send(col + JSON.stringify(return_array));
+            });
         }
         else if( msg == "_Users")
         {
-            ws.send(col + "Il numero di utenti nel db è: " + r.length );
+            require.get({
+                url: "http://localhost:8888/user",
+                header: {
+                    'Content-Type' : 'application/json; charset=utf-8'
+                },
+            }, (err, res, body ) => {
+
+                if(err)
+                    console.log(err)
+
+                var i = 0;
+                for( var ob of JSON.parse(body) )
+                {
+                    i++;
+                }
+                ws.send(col + "Il numero di utenti nel db è: " + i);
+            });
         }
         else
         {
-            ws.send(col + ": Prova con questi comandi: "+ "\n" +" - _AvgPlaylists"+ "\n" +" - _AvgElements"+ "\n" +" - _Categories"+ "\n" +" - _Users");
+            ws.send(col + ": Prova con questi comandi:");
+            ws.send("-> _AvgPlaylists");
+            ws.send("-> _AvgElements");
+            ws.send("-> _Categories");
+            ws.send("-> _Users");
         }
     })
 });
