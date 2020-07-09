1e0851bd3ccf9b44c94e7016082476489dc23309
ciprodakama <telinoiu.1796513@studenti.uniroma1.it>
2020-07-09 16:20:03 +0200

    Debugging

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -8,6 +8,7 @@ function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
+    
     for(var i = 0; i <ca.length; i++) {
       var c = ca[i];
       while (c.charAt(0) == ' ') {
@@ -26,22 +27,30 @@ $(document).ready(function() {
         if(!$("#inputEmail4").val()){
             alert("Per poter procedere devi inserire la tua mail!");
         }
-        else{
-            $.get(url+mail, function(data) {
+        else{ 
+            $.get(url+mail, function() {
+            }).done(function(data){
+                console.log("Questo è l'url da Data\n")
                 console.log(data.url)
+                console.log("Questo è l'id_us da Data\n")
                 console.log(data.id)
                 //document.cookie = "id="+data.id+"; path=/";
+                console.log("Questo è il cookie prima dell'if\n")
                 console.log(getCookie("id"))
                 if (getCookie("id") != data.id){
-                    document.cookie = "id="+data.id+"; path=/";
+                    document.cookie = "id="+data.id+"; path=/; "
+                    console.log("Questo è il cookie dopo l'if\n")
+                    console.log(getCookie("id"))
                     window.location.href = data.url;
                 }
                 else{
+                    console.log("Questo è il cookie nell'else\n")
+                    console.log(getCookie("id"))
                     window.location.href = data.url;
-                }
+                }  
             }).fail(function(){
                 alert("Errore! Non è stato possibile autentificarsi con Google.");
             });
         }
-    });
+    })
 });
